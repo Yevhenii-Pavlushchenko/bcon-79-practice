b7b123884723ef9fc2c1daf9293d2908ed73830e
JustFun <paw93@ukr.net>
2026-01-07 19:14:47 +0200

    finish all 20 test

---
+++ module-01/lesson-01/script.js
@@ -0,0 +1,82 @@
+//1
+const userName = "JEKA";
+console.log(userName);
+// 2
+const birthMonth = 9;
+console.log(birthMonth);
+//3
+const invoiceText = "Ласкаво " + "просимо " + "на курс";
+console.log(invoiceText);
+//4
+// alert(2019);
+//5
+console.log(2019 - 200);
+//6
+const containerEl = document.getElementById("six");
+console.log(containerEl);
+containerEl.textContent = "Hello World";
+//7
+const boxEl = document.getElementById("seven");
+console.log(boxEl);
+boxEl.textContent = 12 * 12;
+//8
+const divEL = document.querySelector(".out-8");
+console.log(divEL);
+divEL.textContent = "task-8";
+//9
+const out9EL = document.querySelector(".out-9");
+console.log(out9EL);
+out9EL.textContent = "world";
+//10
+const out10El = document.querySelector(".out-10");
+console.log(out10El);
+out10El.textContent = "Hi";
+//11
+const out11El = document.querySelector(".out-11");
+console.log(out11El);
+out11El.textContent = "123";
+out11El.innerHTML += "456";
+//12
+const out12El = document.querySelector(".out-12");
+const a = 3.1415;
+console.log(out12El);
+out12El.innerHTML = a;
+//13
+const out13El = document.querySelector(".out-13");
+console.log(out13El);
+out13El.innerHTML =
+  '<img src="https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a" alt="js" width="400" >';
+//14
+const out14El = document.querySelector(".out-14");
+const z1 = 6;
+const z2 = 3;
+console.log(out14El);
+out14El.innerHTML = z1 * z2;
+//15
+const out15El = document.querySelector(".out-15");
+const y1 = 6;
+const y2 = 3;
+console.log(out15El);
+out15El.innerHTML = y1 / y2;
+//16
+const out16El = document.querySelector(".out-16");
+const x1 = "Hello";
+const x2 = 5;
+console.log(out16El);
+out16El.innerHTML = x1 + x2;
+//17
+const out17El = document.querySelector(".out-17");
+console.log(out17El);
+//18
+const out18El = document.querySelector(".out-18");
+console.log(out18El);
+out18El.innerHTML = 5;
+// 19
+const out19El = document.querySelector(".out-19");
+console.log(out19El);
+const out19ElTest = document.querySelector(".out-19-test");
+console.log(out19ElTest);
+//20
+const out20El = document.querySelector(".out-20");
+console.log(out20El);
+out20El.textContent = "<h2>Hi</h2>";
